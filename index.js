207e6db9dc6c87768b4028c294c0158c1347fb14
Franz Bubel <Franz.Bubel@ts.fujitsu.com>
2019-12-31 12:27:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const Market = require('./steam-market-helper');
+
+const market = new Market();
+market.test();
---
+++ steam-market-helper.js
@@ -0,0 +1,8 @@
+class SteamMarketHelper {
+  test() {
+    this.a = 1;
+    console.log(this.a);
+  }
+}
+
+module.exports = SteamMarketHelper;
